6892e9cff43eeb167a5837a134a1a2cb83286091
masaki-0524 <0hg332577251b6c@gmail.com>
2019-09-19 17:29:01 +0900

    fixed comment

--- app/Controller/comment/index.php
+++ app/Controller/comment/index.php
@@ -5,42 +5,22 @@ use Slim\Http\Response;
 use Model\Dao\Comment;
 
 // comment/addページのコントローラ
-$app->get('/comment/add', function (Request $request, Response $response) {
-
-    $data = [];
-
-    // Render index view
-    return $this->view->render($response, 'comment/add.twig', $data);
-});
-
-// comment/detailページのコントローラ
 $app->get('/comment/{plan_id}', function (Request $request, Response $response, $args) {
 
     $data = [];
     $plan_id = $args["plan_id"];
 
-    $comment = new Comment($this->db);
-
-    $result = $comment->select($param,"","",5,true);
-dd($result);
-
-
     // Render index view
-    return $this->view->render($response, 'comment/detail.twig', $data);
+    return $this->view->render($response, 'comment/add.twig', $data);
 });
+
 
 // comment/doneページのコントローラ
 $app->post('/comment/done', function (Request $request, Response $response) {
 
-    $data = [];
-    $data["id"] = 6;
-    $data["user_id"] = 1;
-    $data["plan_id"] = 1;
+    $data = $request->getParsedBody();
+    $data["user_id"] = $this->session["user_info"]["id"] ;
     $data["create_date"] = date("Y-m-d H:i:s");
-    $data["text"] = "面白そう";
-
-
-
 
 
 
