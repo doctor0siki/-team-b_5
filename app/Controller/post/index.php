86f7d90562d8f3b5d8ab60c7c0909b51c1c0dd96
h-yamagishi-02 <hiro.gishi.go@gmail.com>
2019-09-19 18:01:15 +0900

    fixed 入力フォーム

--- app/Controller/post/index.php
+++ app/Controller/post/index.php
@@ -5,23 +5,23 @@ use Slim\Http\Response;
 use Model\Dao\Plan;
 
 // TOPページのコントローラ
-$app->post('/post', function (Request $request, Response $response) {
+$app->get('/post', function (Request $request, Response $response) {
+dd("aaaaa");
 
-    $data = [];
-    $data["id"] = "";
-    $data["title"] = "";
-    $data["sub-title"] = "";
-    $data["detail"] = "";
-    $data["place"] = "";
-    $data["cat_code"] = "";
-    $data["create_date"] = "";
-    $data["user_id"] = "";
-    $data["good"] = "";
-    $data["img_url"] = "";
-    $data["visit_date"] = "";
-    $plan = new Plan($this->db);
+
     $result = $plan->search_plan($data);
-    dd($result);
+
+    // Render index view
+    return $this->view->render($response, 'post/input.twig', $data);
+});
+
+$app->get('/post/done', function (Request $request, Response $response) {
+dd("aaaaa");
+
+    $data = $request->getParsedBody();
+    
+    $result = $plan->search_plan($data);
+
     // Render index view
     return $this->view->render($response, 'post/input.twig', $data);
 });
