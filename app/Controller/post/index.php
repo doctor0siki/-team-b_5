4bbe58ecebc96a08632629b5c3a019f5d572b39a
y-fukumoto <y-fukumoto@ceres-inc.jp>
2019-09-19 19:49:28 +0900

    change column name

---
+++ app/Controller/post/index.php
@@ -0,0 +1,24 @@
+<?php
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Model\Dao\Plan;
+
+// TOPページのコントローラ
+$app->get('/post', function (Request $request, Response $response) {
+
+    $data = [];
+
+    // Render index view
+    return $this->view->render($response, 'post/add.twig', $data);
+});
+
+$app->post('/post/done', function (Request $request, Response $response) {
+
+    $data = $request->getParsedBody();
+    $plan = new Plan($this->db);
+    $id = $plan->insert($data);
+
+    // Render index view
+    return $this->view->render($response, 'post/done.twig', $data);
+});
