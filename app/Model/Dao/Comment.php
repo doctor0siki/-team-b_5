15c1441dc23e127a33a54fa8779480010ef24528
pmg0738 <pmg0738@naver.com>
2019-09-20 15:19:18 +0900

    link to add comment link to detail plan

--- app/Model/Dao/Comment.php
+++ app/Model/Dao/Comment.php
@@ -15,6 +15,19 @@ namespace Model\Dao;
  */
 class Comment extends Dao
 {
+  public function get_detailcomment($planid){
+    $sql = "select * from comment left join user on comment.user_id = user.id where comment.plan_id=:planid";
+    // SQLをプリペア
+    $statement = $this->db->prepare($sql);
 
+    $statement->bindValue(":planid", $planid, \PDO::PARAM_INT);
+    //SQLを実行
+    $statement->execute();
+
+    $result = $statement->fetchAll();
+
+    return $result;
+
+  }
 
 }
